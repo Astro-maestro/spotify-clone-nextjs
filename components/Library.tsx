efbc486d8ac210f247c285cb236c6c01214829ba
Ayush Ranjan Sinha <asayushranjansinha@gmail.com>
2023-07-31 10:15:25 +0530

    feat:upload songs

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,34 +1,39 @@
-'use client';
+"use client";
 
-import {TbPlaylist} from 'react-icons/tb'
-import {AiOutlinePlus} from 'react-icons/ai'
+import { TbPlaylist } from "react-icons/tb";
+import { AiOutlinePlus } from "react-icons/ai";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
 
 const Library = () => {
-    
-    const onClick = ()=>{
-        // TODO:handle upload files later
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user, subscription } = useUser();
+  const onClick = () => {
+    if (!user) {
+      authModal.onOpen();
+      return;
     }
+    return uploadModal.onOpen();
+  };
 
-
-    return (
+  return (
     <div className="flex flex-col">
-        <div className="flex items-center justify-between px-5 pt-4">
-
-            <div className="inline-flex items-center gap-x-2">
-                <TbPlaylist size={26} className='text-neutral-400'/>
-                <p className='text-neutral-400 font-medium'>Your Library</p>
-            </div>
-            <AiOutlinePlus 
-                onClick={()=>{}}
-                className='text-neutral-400 cursor-pointer hover:text-white transition'
-            />
+      <div className="flex items-center justify-between px-5 pt-4">
+        <div className="inline-flex items-center gap-x-2">
+          <TbPlaylist size={26} className="text-neutral-400" />
+          <p className="text-neutral-400 font-medium">Your Library</p>
         </div>
+        <AiOutlinePlus
+          onClick={onClick}
+          className="text-neutral-400 cursor-pointer hover:text-white transition"
+        />
+      </div>
 
-        <div className="flex flex-col gap-y-2 mt-4 px-3">
-            List of Songs
-        </div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">List of Songs</div>
     </div>
-  )
-}
+  );
+};
 
-export default Library
+export default Library;
