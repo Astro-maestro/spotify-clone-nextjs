a764ec3bdda15d0e3dedd961459b0b4c28c5c0c7
Ayush Ranjan Sinha <asayushranjansinha@gmail.com>
2023-07-31 19:33:44 +0530

    feat:like songs

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -2,8 +2,10 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-
 import { FaPlay } from "react-icons/fa";
+
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
 
 interface ListItemProps {
   image: string;
@@ -11,28 +13,73 @@ interface ListItemProps {
   href: string;
 }
 
-const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
+const ListItem: React.FC<ListItemProps> = ({
+  image,
+  name,
+  href,
+}) => {
   const router = useRouter();
-
+  const authModal = useAuthModal();
+  const { user } = useUser();
+  
   const onClick = () => {
-    // TODO:auth
+    if (!user) {
+      return authModal.onOpen();
+    }
+
     router.push(href);
   };
-  return (
+
+  return ( 
     <button
-      className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4"
-      onClick={() => {}}
+      onClick={onClick}
+      className="
+        relative 
+        group 
+        flex 
+        items-center 
+        rounded-md 
+        overflow-hidden 
+        gap-x-4 
+        bg-neutral-100/10 
+        cursor-pointer 
+        hover:bg-neutral-100/20 
+        transition 
+        pr-4
+      "
     >
       <div className="relative min-h-[64px] min-w-[64px]">
-        <Image src={image} alt={name} fill className="object-cover" />
+        <Image
+          className="object-cover"
+          src={image}
+          fill
+          alt="Image"
+        />
       </div>
-      <p className="font-medium truncate py-5">{name}</p>
-
-      <div className="absolute transition opacity-0 rounded-full flex items-center justify-between bg-emerald-500 p-4 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110">
+      <p className="font-medium truncate py-5">
+        {name}
+      </p>
+      <div 
+        className="
+          absolute 
+          transition 
+          opacity-0 
+          rounded-full 
+          flex 
+          items-center 
+          justify-center 
+          bg-green-500 
+          p-4 
+          drop-shadow-md 
+          right-5
+          group-hover:opacity-100 
+          hover:scale-110
+        "
+      >
         <FaPlay className="text-black" />
       </div>
     </button>
-  );
-};
-
+   );
+}
+ 
 export default ListItem;
