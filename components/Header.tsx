370892014dd349b4db34455ef208ce2c77f2b7cd
Ayush Ranjan Sinha <asayushranjansinha@gmail.com>
2023-08-02 18:17:53 +0530

    final commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,17 @@
 "use client";
-import { useRouter } from "next/navigation";
-import { toast } from "react-hot-toast";
 
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { useRouter } from "next/navigation";
+import { FaUserAlt } from "react-icons/fa";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { toast } from "react-hot-toast";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
-import { FaUserAlt } from "react-icons/fa";
 
 import useAuthModal from "@/hooks/useAuthModal";
-import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useUser } from "@/hooks/useUser";
+import usePlayer from "@/hooks/usePlayer";
 
 import Button from "./Button";
 
@@ -20,56 +21,102 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const player = usePlayer();
   const router = useRouter();
   const authModal = useAuthModal();
 
   const supabaseClient = useSupabaseClient();
   const { user } = useUser();
+
   const handleLogout = async () => {
     const { error } = await supabaseClient.auth.signOut();
+    player.reset();
     router.refresh();
 
-    if (error){
+    if (error) {
       toast.error(error.message);
     }
-    else{
-      toast.success("Logged out!")
-    }
   };
+
   return (
     <div
       className={twMerge(
-        "h-fit bg-gradient-to-b from-emerald-900 p-6",
+        `
+        h-fit 
+        bg-gradient-to-b 
+        from-emerald-800 
+        p-6
+        `,
         className
       )}
     >
       <div className="w-full mb-4 flex items-center justify-between">
-        {/* navigation desktop */}
         <div className="hidden md:flex gap-x-2 items-center">
           <button
-            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
             onClick={() => router.back()}
+            className="
+              rounded-full 
+              bg-black 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
           >
-            <RxCaretLeft size={35} />
+            <RxCaretLeft className="text-white" size={35} />
           </button>
           <button
-            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
             onClick={() => router.forward()}
+            className="
+              rounded-full 
+              bg-black 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
           >
-            <RxCaretRight size={35} />
+            <RxCaretRight className="text-white" size={35} />
           </button>
         </div>
-
-        {/*navigation mobile */}
         <div className="flex md:hidden gap-x-2 items-center">
-          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
-            <HiHome size={20} className="text-black" />
+          <button
+            onClick={() => router.push("/")}
+            className="
+              rounded-full 
+              p-2 
+              bg-white 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
+          >
+            <HiHome className="text-black" size={20} />
           </button>
-          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
-            <BiSearch size={20} className="text-black" />
+          <button
+            onClick={() => router.push("/search")}
+            className="
+              rounded-full 
+              p-2 
+              bg-white 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
+          >
+            <BiSearch className="text-black" size={20} />
           </button>
         </div>
-
         <div className="flex justify-between items-center gap-x-4">
           {user ? (
             <div className="flex gap-x-4 items-center">
